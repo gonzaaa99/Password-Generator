6e9dea2935b226f02fadd1d8f9d1966cf1b0cb03
gonzaaa99 <gondelpozo@gmail.com>
2021-01-30 23:55:51 +0100

    apariencia

--- password_generator.py
+++ password_generator.py
@@ -12,30 +12,46 @@ class PasswordGenerator():
         """
         Initializes the Tkinter interface
         """
+        #Constantes
+        bg_color    = 'light gray'
+        bg_button   = 'light blue'
+        bg_ckbutton = 'light gray'
+        bg_entry    = 'light gray'
+        bg_label    = 'light gray'
+        padx         = 10
+        pady         = 10
+
+        #Ventana principal
         self.root = Tk()
         self.root.title('Password Generator')
+        self.root.iconbitmap('Resources/Images/icon.ico')
+        self.root.configure(background = bg_color)
 
         #Entry
-        self.length = ttk.Entry(self.root,text = 'length')                              #Length
-        self.length.grid(row = 0,column = 1)
+        self.length = ttk.Entry(self.root,text = 'length', background = bg_entry)    #Length
+        self.length.grid(row = 0,column = 1,pady=pady,padx=padx)
         self.length.insert(0,'8')
         self.length.focus()
-        self.generatedPassword = ttk.Entry(self.root)                                   #Generated password
-        self.generatedPassword.grid(row = 3,column = 0,columnspan = 3,sticky = 'e w')
+        self.generatedPassword = ttk.Entry(self.root, background = bg_entry)         #Generated password
+        self.generatedPassword.grid(row = 3,column = 0,columnspan = 3,sticky = 'e w', pady=pady,padx=padx)
 
         #Label
-        Label(self.root,text='Length:').grid(row=0,column=0)
+        Label(self.root,background= bg_label,text='Length:').grid(row=0,column=0)
 
         #Button
-        Button(self.root,text='Generate password',command = lambda: self.generatePassword()).grid(row=2,column=0,columnspan=3)
+        Button( self.root,
+                text = 'Generate password',
+                background =  bg_button,
+                command = lambda: self.generatePassword(),
+                ).grid(row=2,column=0,columnspan=3)
 
         #Check Buttons
         self.letters_var     = IntVar()     # 1 yes, 0 no
         self.digits_var      = IntVar()     # 1 yes, 0 no
         self.punctuation_var = IntVar()     # 1 yes, 0 no
-        letters_chkbtn =     Checkbutton(self.root, text="letters",     variable=self.letters_var,       onvalue=1, offvalue=0)
-        digits_chkbtn =      Checkbutton(self.root, text="digits",      variable=self.digits_var,        onvalue=1, offvalue=0)
-        punctuation_chkbtn = Checkbutton(self.root, text="punctuation", variable=self.punctuation_var,   onvalue=1, offvalue=0)
+        letters_chkbtn =     Checkbutton(self.root, text="letters",     variable=self.letters_var,       onvalue=1, offvalue=0, background = bg_ckbutton)
+        digits_chkbtn =      Checkbutton(self.root, text="digits",      variable=self.digits_var,        onvalue=1, offvalue=0, background = bg_ckbutton)
+        punctuation_chkbtn = Checkbutton(self.root, text="punctuation", variable=self.punctuation_var,   onvalue=1, offvalue=0, background = bg_ckbutton)
         letters_chkbtn.select()
         digits_chkbtn.select()
         punctuation_chkbtn.select()
